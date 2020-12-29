c48e9cbb1459096bc0c8e1c4a91e02eb1bca3106
adi <75527550+adi2796@users.noreply.github.com>
2020-12-29 22:51:47 +0530

    Create ListReplication.scala

---
+++ ListReplication.scala
@@ -0,0 +1,55 @@
+
+Given a list, repeat each element in the list  amount of times. The input and output portions will be handled automatically by the grader. You need to write a function with the recommended method signature.
+
+Input Format
+
+The first line contains the integer  where  is the number of times you need to repeat the elements.
+The next  lines each contain an integer. These are the  elements in the array.
+
+Output Format
+
+Output each element of the original list  times, each on a separate line. You have to return the list/vector/array of  integers. The relative positions of the values should be the same as the original list provided in the input.
+
+Constraints
+
+
+
+Sample Input
+
+3
+1
+2
+3
+4
+Sample Output
+
+1
+1
+1
+2
+2
+2
+3
+3
+3
+4
+4
+4
+------------------------------------------------------------------
+import scala.collection.mutable.ListBuffer
+
+def f(num:Int,arr:List[Int]):List[Int] ={
+    
+var a = new ListBuffer[Int]()
+   val l=arr.length
+    for(i <- 0 to l-1)
+    { 
+        var c=num
+        
+        while(c>0){
+         a+=(arr(i))
+         c=c-1
+        }
+    }
+    return a.toList
+}
